a4199a48022930bb82694ea29f63e5c005007afc
unknown <dario.car.kz@gmail.com>
2021-02-01 20:24:56 +0100

    update

--- 03-js-types-string-exercise/index.js
+++ 03-js-types-string-exercise/index.js
@@ -5,23 +5,24 @@
  * first - greeting, second - name and return it as one string
  * separated with space(" ")
  */
-
+const greetGenerator = (greeting, name) => greeting + " " + name;
 /**
  * Exercise 2
  * 
  * create a function "convertToLowerCase" which takes a capitalized string
  * as a param, convert to lowercase string and return it.
  */
-
+const convertToLowerCase = (UpperCase) => UpperCase.toLowerCase();
 /**
  * Exercise 3
  * 
  * create a function "convertToUpperCase" which takes a lowercase string
  * for example('kitten') as a param, convert to uppercase string('KITTEN') and return it.
  */
-
+const convertToUpperCase = (LowerCase) => LowerCase.toUpperCase();
 /**
  * Exercise 4
  * create a function 'getCharByIndex' that takes a string and a character position
  * as a params and return a character from your string that is in selected position
  */
+const getCharByIndex = (string, indexOf()) => return indexOf
