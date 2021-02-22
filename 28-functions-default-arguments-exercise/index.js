1819976169fc27bafb7ea5b1b7849b36144492ba
unknown <dario.car.kz@gmail.com>
2021-02-22 16:12:58 +0100

    getting there

--- 28-functions-default-arguments-exercise/index.js
+++ 28-functions-default-arguments-exercise/index.js
@@ -1,40 +1,42 @@
 /**
  * Exercise 1
- * 
+ *
  * create a function {sayHi} which takes param
- * {name} and return 'Hi, {name}!' if you don't pass any 
- * param it should use default value for name which 
+ * {name} and return 'Hi, {name}!' if you don't pass any
+ * param it should use default value for name which
  * should be 'incognito'
  */
-const sayHi = (name = 'incognito') => {return `Hi, ${name}!`};
+const sayHi = (name = "incognito") => {
+  return `Hi, ${name}!`;
+};
 console.log(sayHi(`Marko`));
 console.log(sayHi());
 /**
  * Exercise 2
- * 
+ *
  * create a function {multiply} which takes 2 numbers
  * as params, multiply them and return a result.
- * 
- * It you don't pass 1 or both params it should use 1 as 
+ *
+ * It you don't pass 1 or both params it should use 1 as
  * a default value/values
  */
 
- const multiply = (x = 1, y = 1) => x * y;
+const multiply = (x = 1, y = 1) => x * y;
 
 /**
  * Exercise 3
  * create a function {logElements} that takes array as a param
- * by looping through it log every element. If you don't 
- * pass any array, default value of your param should be 
+ * by looping through it log every element. If you don't
+ * pass any array, default value of your param should be
  * array that contains only one element - string with value "Nothing to log"
  */
 
- const logElements = (array = [`Nothing to log`]) => {
-     for (let i = 0; i < array.length; i++) {
-         console.log(array[i]);
-     }
- }
- console.log(logElements([`a`, `b`, `c`, `d`]));
+const logElements = (array = [`Nothing to log`]) => {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+};
+console.log(logElements([`a`, `b`, `c`, `d`]));
 
 /**
  * Exercise 4
@@ -44,5 +46,5 @@ console.log(sayHi());
  * and value "John Smith"
  */
 
- const getUserName = ({name} = {name: `John Smith`}) => name;
- console.log(getUserName(`Marko Slavecki`));
+const getUserName = ({ name } = { name: `John Smith` }) => name;
+console.log(getUserName(`Marko Slavecki`));
--- 29-inline-functions-exercise/index.js
+++ 29-inline-functions-exercise/index.js
@@ -10,14 +10,27 @@
  * use default value "[]", is you pass any other type than array
  * return a string "Please, pass correct type".
  */
-
+const reverseArray = function (array = []) {
+  if (Array.isArray(array)) {
+    return array.reverse();
+  } else {
+    return `Please, pass correct type`;
+  }
+};
 /**
  * Exercise 2
  *
  * create an inline function {nextOddNum} that takes number as a param
  * and return next odd number.
  */
-
+const nextOddNum = function (num) {
+  if (num % 2 === 0) {
+    return num + 1;
+  } else {
+    return num + 2;
+  }
+};
+console.log(nextOddNum(4));
 /**
  * Exercise 3
  *
@@ -26,12 +39,23 @@
  * Given two numbers, return true if the sum of both numbers is
  * less than 100. Otherwise return false.
  */
-
+const isLessThan100 = function (num1, num2) {
+  if (num1 + num2 < 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isLessThan100(50, 439));
 /**
  * Exercise 4
  * create an inline function {incrementBy5} which takes
  * a number as a param, add 5 and return the result.
  */
+
+const incrementBy5 = function (num) {
+  return num + 5;
+};
 
 /**
  * Exercise 5
@@ -40,3 +64,9 @@
  * Then apply incrementor function to each element in your array and
  * return the result
  */
+const incrementAllBy5 = function (arr, incrementBy5) {
+  let newArr = [];
+  newArr = arr.map(incrementBy5);
+  return newArr;
+};
+console.log(incrementAllBy5([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], incrementBy5));
