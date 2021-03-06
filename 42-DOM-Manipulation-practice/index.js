232e54dbdf34864fdd7e4faeb4eac4079bd985a9
unknown <dario.car.kz@gmail.com>
2021-03-06 19:33:27 +0100

    42 was tricky, but Vasile helped

--- 42-DOM-Manipulation-practice/index.js
+++ 42-DOM-Manipulation-practice/index.js
@@ -8,7 +8,21 @@
  * 3. you should add only 5 list items.
  * 4. list item text should be "Item $"($ - position in the list)
  */
+const createAList = () => {
+  const body = document.querySelector("body");
+  const newList = document.createElement("ul");
+  newList.classList.add("list");
 
+  for (let i = 0; i < 5; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerText = "Item " + (i + 1);
+    setTimeout(() => {
+      newList.appendChild(listItem);
+    }, 1000 * i);
+  }
+
+  body.appendChild(newList);
+};
 /**
  * Exercise 2
  *
@@ -20,7 +34,12 @@
  * 3. set color to "white"
  * 4. set font size to 2em
  */
-
+const styleElement = () => {
+  const third = document.querySelector(".myList li:nth-child(3)");
+  third.style.backgroundColor = "green";
+  third.style.color = "white";
+  third.style.fontSize = "2em";
+};
 /**
  * Exercise 3
  *
@@ -29,7 +48,11 @@
  * 1. select last element from the ".myList"
  * 2. wait 2 seconds and remove the element from the list
  */
-
+const removeLastChild = () => {
+  setTimeout(() => {
+    document.querySelector(".myList li:last-child").remove();
+  }, 2000);
+};
 /**
  * Exercise 4
  *
@@ -46,3 +69,19 @@
  * NOTE: check css file to see how we toggle styles
  * based on class "visible"
  */
+const createAMessage = text => {
+  const body = document.querySelector("body");
+  const pTag = document.createElement("p");
+  pTag.classList.add("message");
+  pTag.innerText = text;
+  body.appendChild(pTag);
+  setTimeout(() => {
+    pTag.classList.add("visible");
+  }, 3000);
+  setTimeout(() => {
+    pTag.classList.add("hide");
+  }, 6000);
+  setTimeout(() => {
+    pTag.remove();
+  }, 8000);
+};
