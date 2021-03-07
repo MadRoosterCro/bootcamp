3ad2548c8345856d5dfdff3b118c0ac8dca84054
unknown <dario.car.kz@gmail.com>
2021-03-07 01:46:29 +0100

    44 ok...

--- 44-other-listeners/index.js
+++ 44-other-listeners/index.js
@@ -5,6 +5,11 @@
  * a popup(add class "open" to "popup")
  * when you double click on ".trigger"
  */
+const popup = document.querySelector(".popup");
+const trigger = document.querySelector(".trigger");
+trigger.addEventListener("dblclick", () => {
+  popup.classList.add("open");
+});
 
 /**
  * Exercise 2
@@ -13,13 +18,25 @@
  * zoom class on image when you double click on
  * image container(".img")
  */
-
+const imgContainer = document.querySelector(".img");
+const img = document.querySelector("img");
+imgContainer.addEventListener("dblclick", () => {
+  img.classList.toggle("zoom");
+});
 
 /**
  * Exercise 3
  *
  * create an event listener which will stop/start music player
  * when you switch tab/come back to the tab in the browser.
- * 
+ *
  * NOTE: to test please interact with the page, and switch tabs
  */
+const musicPlayer = document.querySelector("audio");
+
+window.addEventListener("blur", () => {
+  musicPlayer.pause();
+});
+window.addEventListener("focus", () => {
+  musicPlayer.play();
+});
