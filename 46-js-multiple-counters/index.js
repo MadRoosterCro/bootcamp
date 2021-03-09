13b6ee3d5a6ba6890f43fff8e07e6535aaeb34ff
unknown <dario.car.kz@gmail.com>
2021-03-09 10:18:10 +0100

    progress

--- 46-js-multiple-counters/index.js
+++ 46-js-multiple-counters/index.js
@@ -14,3 +14,24 @@ let timersCount = 0;
  * NOTE: each timer should have additional class with unique number(as you
  * can see in example - timer_$)
  */
+
+const createTimerEl = (number) => {
+  const divForTimer = document.createElement("div");
+  let cunter = 0;
+  divForTimer.classList.add(`timer`, `timer_${number}`);
+  divForTimer.innerHTML = `
+  <div class = "remove"></div>
+  <h3>0 <span>00</span></h3>
+  <div class="btn">
+    <button class="reset">Reset</button>
+    <button class="stop">Stop</button>
+  </div>
+</div>
+`
+
+const removeButton = divForTimer.querySelector(`.remove`);
+const timerSpan = divForTimer.querySelector(`.h3`);
+const resetButton = divForTimer.querySelector(`.reset`);
+const stopButton = divForTimer.querySelector(`.stop`);
+
+}
