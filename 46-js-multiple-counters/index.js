f3a21d415845c7b7c63e4ec2466e82e165ccd3dd
unknown <dario.car.kz@gmail.com>
2021-03-09 10:41:33 +0100

    progress

--- 46-js-multiple-counters/index.js
+++ 46-js-multiple-counters/index.js
@@ -61,37 +61,3 @@ addCounterEl.addEventListener("click", () => {
 
 //
 // const createTimerEl = (number) => {
-//   const divForTimer = document.createElement("div");
-//   let counter = 0;
-//   divForTimer.classList.add(`timer`, `timer_${number}`);
-//   divForTimer.innerHTML = `
-//   <div class = "remove"></div>
-//   <h3>0 <span>00</span></h3>
-//   <div class="btn">
-//     <button class="reset">Reset</button>
-//     <button class="stop">Stop</button>
-//   </div>
-// </div>
-// `
-//
-// const removeButton = divForTimer.querySelector(`.remove`);
-// const timerSpan = divForTimer.querySelector(`.h3`);
-// const resetButton = divForTimer.querySelector(`.reset`);
-// const stopButton = divForTimer.querySelector(`.stop`);
-
-// const timerInterval = setInterval(() => {
-//     const seconds = Math.floor(countr/100);
-//     const milliseconds = counter % 100;
-//     timerSpan.innerHTML = `${seconds}<span>${milliseconds}</span>`;
-//     counter++;
-// }, 10);
-
-// removeButton.addEventListener("click", () => {
-//     clearInterval(timerInterval);
-//     divForTimer.remove();
-// });
-// resetButton.addEventListener("click", () => counter = 0);
-// stopButton.addEventListener("click", () => {
-//     clearInterval(timerInterval);
-//     return divForTimer;
-// });
