8c23c24b3e421df31dc4e8c09935d5222c385730
unknown <dario.car.kz@gmail.com>
2021-03-09 10:41:19 +0100

    progress

--- 46-js-multiple-counters/README.md
+++ 46-js-multiple-counters/README.md
@@ -1,37 +1,45 @@
-# Multiple stopwatches
 
-## Demo GIF
+// const createTimerEl = (number) => {
+//   const divForTimer = document.createElement("div");
+//   let counter = 0;
+//   divForTimer.classList.add(`timer`, `timer_${number}`);
+//   divForTimer.innerHTML = `
+//   <div class = "remove"></div>
+//   <h3>0 <span>00</span></h3>
+//   <div class="btn">
+//     <button class="reset">Reset</button>
+//     <button class="stop">Stop</button>
+//   </div>
+// </div>
+// `
+//
+// const removeButton = divForTimer.querySelector(`.remove`);
+// const timerSpan = divForTimer.querySelector(`.h3`);
+// const resetButton = divForTimer.querySelector(`.reset`);
+// const stopButton = divForTimer.querySelector(`.stop`);
 
-![demo gif](./example.gif)
+// const timerInterval = setInterval(() => {
+//     const seconds = Math.floor(countr/100);
+//     const milliseconds = counter % 100;
+//     timerSpan.innerHTML = `${seconds}<span>${milliseconds}</span>`;
+//     counter++;
+// }, 10);
 
-## Deliverables
-
-1. As a user I should be able to add a new timer to the DOM by clicking on "Add Timer".
-2. A timer should continuously count up and always show seconds and the first two integers of milliseconds
-3. When I click "Reset", that timer should return to 0.
-4. When I click "Stop", it should stop that timer.
-5. When I click "X", the timer should stop and be removed from the DOM
-6. I should be able to add NO MORE than 5 timers.
-
-## How to approach it
-
-There's much less guidance in the README than usual. One approach might be to plan all the functions you think you'll need (more small functions is _probably_ better than fewer big functions!), then write your own descriptions of what they might do. Then figure out how to write the code to achieve what the descriptions say.
-
-Before you start coding, read all the deliverables listed above. Yes, **all** of them!
-
-Once you've started coding, don't move on to the next deliverable until you've completed the one you're currently working on.
-
-After you complete each deliverable, test everything again in the browser. You never know what you might have broken with your new code!
-
-## Timer HTML Structure
-
-```html
-<div class="timer timer_1">
-  <div class="remove"></div>
-  <h3>0 <span>00</span></h3>
-  <div class="btn">
-    <button class="reset">Reset</button>
-    <button class="stop">Stop</button>
-  </div>
-</div>
-```
+// removeButton.addEventListener("click", () => {
+//     clearInterval(timerInterval);
+//     divForTimer.remove();
+// });
+// resetButton.addEventListener("click", () => counter = 0);
+// stopButton.addEventListener("click", () => {
+//     clearInterval(timerInterval);
+//     return divForTimer;
+// });
+// addCounterEl.addEventListener("click", () => {
+//     const numberOfTimersAdded = document.querySelectorAll(".timer").length
+//     if (numberOfTimersAdded < 5) {
+//         timersCount++;
+//         const newTimer = createTimerEl(timersCount);
+//         timersContainerEl.appendChild(newTimer);
+//     }
+// })
+// }
--- 46-js-multiple-counters/index.js
+++ 46-js-multiple-counters/index.js
@@ -14,48 +14,84 @@ let timersCount = 0;
  * NOTE: each timer should have additional class with unique number(as you
  * can see in example - timer_$)
  */
-/// first I need to create a timer 
 const createTimerEl = (number) => {
   const divForTimer = document.createElement("div");
   let counter = 0;
-  divForTimer.classList.add(`timer`, `timer_${number}`);
+  divForTimer.classList.add("timer", `timer_${number}`);
   divForTimer.innerHTML = `
-  <div class = "remove"></div>
-  <h3>0 <span>00</span></h3>
-  <div class="btn">
-    <button class="reset">Reset</button>
-    <button class="stop">Stop</button>
-  </div>
-</div>
-`
+      <div class="remove"></div>
+    <h3>0 <span>00</span></h3>
+    <div class="btn">
+      <button class="reset">Reset</button>
+      <button class="stop">Stop</button>
+    </div>
+    `;
 
-const removeButton = divForTimer.querySelector(`.remove`);
-const timerSpan = divForTimer.querySelector(`.h3`);
-const resetButton = divForTimer.querySelector(`.reset`);
-const stopButton = divForTimer.querySelector(`.stop`);
+  const removeButton = divForTimer.querySelector(".remove");
+  const timerSpan = divForTimer.querySelector("h3");
+  const resetButton = divForTimer.querySelector(".reset");
+  const stopButton = divForTimer.querySelector(".stop");
 
-const timerInterval = setInterval(() => {
-    const seconds = Math.floor(countr/100);
+  const timerInterval = setInterval(() => {
+    const seconds = Math.floor(counter / 100);
     const milliseconds = counter % 100;
     timerSpan.innerHTML = `${seconds}<span>${milliseconds}</span>`;
-    counter++; 
-}, 10);
-
-removeButton.addEventListener("click", () => {
+    counter++;
+  }, 10);
+  removeButton.addEventListener("click", () => {
     clearInterval(timerInterval);
     divForTimer.remove();
-});
-resetButton.addEventListener("click", () => counter = 0);
-stopButton.addEventListener("click", () => {
-    clearInterval(timerInterval);
-    return divForTimer;
-});
+  });
+  resetButton.addEventListener("click", () => (counter = 0));
+
+  stopButton.addEventListener("click", () => clearInterval(timerInterval));
+
+  return divForTimer;
+};
+
 addCounterEl.addEventListener("click", () => {
-    const numberOfTimersAdded = document.querySelectorAll(".timer").length
-    if (numberOfTimersAdded < 5) {
-        timersCount++;
-        const newTimer = createTimerEl(timersCount);
-        timersContainerEl.appendChild(newTimer);
-    }
-})
-}
+  const numberOfTimersAdded = document.querySelectorAll(".timer").length;
+
+  if (numberOfTimersAdded < 5) {
+    timersCount++;
+    const newTimer = createTimerEl(timersCount);
+    timersContainerEl.appendChild(newTimer);
+  }
+});
+
+//
+// const createTimerEl = (number) => {
+//   const divForTimer = document.createElement("div");
+//   let counter = 0;
+//   divForTimer.classList.add(`timer`, `timer_${number}`);
+//   divForTimer.innerHTML = `
+//   <div class = "remove"></div>
+//   <h3>0 <span>00</span></h3>
+//   <div class="btn">
+//     <button class="reset">Reset</button>
+//     <button class="stop">Stop</button>
+//   </div>
+// </div>
+// `
+//
+// const removeButton = divForTimer.querySelector(`.remove`);
+// const timerSpan = divForTimer.querySelector(`.h3`);
+// const resetButton = divForTimer.querySelector(`.reset`);
+// const stopButton = divForTimer.querySelector(`.stop`);
+
+// const timerInterval = setInterval(() => {
+//     const seconds = Math.floor(countr/100);
+//     const milliseconds = counter % 100;
+//     timerSpan.innerHTML = `${seconds}<span>${milliseconds}</span>`;
+//     counter++;
+// }, 10);
+
+// removeButton.addEventListener("click", () => {
+//     clearInterval(timerInterval);
+//     divForTimer.remove();
+// });
+// resetButton.addEventListener("click", () => counter = 0);
+// stopButton.addEventListener("click", () => {
+//     clearInterval(timerInterval);
+//     return divForTimer;
+// });
