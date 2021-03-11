13e61d3e4ab97b3b60b96b5aef0759accea33fd6
unknown <dario.car.kz@gmail.com>
2021-03-11 14:28:54 +0100

    some js

--- 47-js-pinterest-simple/index.js
+++ 47-js-pinterest-simple/index.js
@@ -1,5 +1,31 @@
 /// firstly I need to tackle like button
 
 const liked = () => {
-    const likedPicture = document.querySelector()
+    const likedPicture = document.querySelector(".like_button>img");
+    const like = () => {
+        likedPicture.src=`.img/like_full.png`
+        likedPicture.className = "liked;"
+    }
+    const dislike = () => {
+        likedPicture.src=`./img/like_empty.png`
+        likedPicture.className = "disliked"
+    }
+    if(likedPicture.classList.contains(`liked`)) {
+        dislike();
+    }
+    else if (likedPicture.classList.contains(`disliked`)) {
+        like();
+    }
 }
+const likeButton = document.querySelector(`.like_button`);
+likeButton.addEventListener("click", () => {
+    liked();
+})
+
+/// comment button section
+const commentButton = document.querySelector(`.comment_button`);
+const newComment = document.querySelector(`.newComment`);
+commentButton.addEventListener("click", () => {
+    newComment.classList.toggle("hide");
+})
+
