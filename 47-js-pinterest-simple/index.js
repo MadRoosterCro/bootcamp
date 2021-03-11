45ca15567c5821bd1255aea22df36b08bf0405b5
unknown <dario.car.kz@gmail.com>
2021-03-11 15:01:33 +0100

    no comment

--- 47-js-pinterest-simple/index.js
+++ 47-js-pinterest-simple/index.js
@@ -1,14 +1,14 @@
 /// firstly I need to tackle like button
 
 const liked = () => {
-  const likedPicture = document.querySelector(".like_button>img");
+  const likedPicture = document.querySelector(`.like_button>img`);
   const like = () => {
     likedPicture.src = `.img/like_full.png`;
-    likedPicture.className = "liked;";
+    likedPicture.className = `liked;`;
   };
   const dislike = () => {
     likedPicture.src = `./img/like_empty.png`;
-    likedPicture.className = "disliked";
+    likedPicture.className = `disliked`;
   };
   if (likedPicture.classList.contains(`liked`)) {
     dislike();
@@ -17,28 +17,28 @@ const liked = () => {
   }
 };
 const likeButton = document.querySelector(`.like_button`);
-likeButton.addEventListener("click", () => {
+likeButton.addEventListener(`click`, () => {
   liked();
 });
 
 /// comment button section
 const commentButton = document.querySelector(`.comment_button`);
 const newComment = document.querySelector(`.newComment`);
-commentButton.addEventListener("click", () => {
-  newComment.classList.toggle("hide");
+commentButton.addEventListener(`click`, () => {
+  newComment.classList.toggle(`hide`);
 });
 
 /// adding new comments to the picture
 
 let commentId = 0;
-const user = "Mad_Rooster";
-let userComment = "This is an amazing bike!";
+const user = `Mad_Rooster`;
+let userComment = `This is an amazing bike!`;
 const addComment = () => {
   const commentContent = document.querySelector(`textarea`).value;
   const structure = document.createElement(`div`);
   structure.id = commentId++;
   commentId = parseInt(structure.id);
-  structure.classList.add("comment");
+  structure.classList.add(`comment`);
   structure.innerHTML = `
     <em>
     <h3>${user}</h3>
