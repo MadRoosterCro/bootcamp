9736cb5dc3ff05070a0a2811091e07aa34b9938c
unknown <dario.car.kz@gmail.com>
2021-03-17 11:43:45 +0100

    add a timeout to error and refactor

--- 49-budget-tracker-exercise/index.js
+++ 49-budget-tracker-exercise/index.js
@@ -1,55 +1,82 @@
 const productsContainer = document.querySelector("#products");
 
-const populateChoices = (choices) => {
-  let allChoices;
-  for (let i = 0; i < choices + 1; i++) {
-    choiceString = "<option value=" + i + ">" + i + "</option>";
-    allChoices = allChoices + choiceString;
+const populateOptions = (options) => {
+  let allOptions = "";
+  for (let i = 0; i < options + 1; i++) {
+    optionString = "<option value=" + i + ">" + i + "</option>";
+    allOptions = allOptions + optionString;
   }
-  return allChoices;
+  return allOptions;
 };
-/// create product elements
 
-produtcs.forEach((product) => {
+///create product elements
+
+products.forEach((product) => {
   const divForProducts = document.createElement("div");
   divForProducts.id = product.id;
   divForProducts.classList.add("product");
-  const moreThanOne = populateChoices(product.max_quantity);
-  divForProducts.innerHTML = `<img src="${product.img}">
-    <h3>${product.name}</h3>
-    <p>${product.price}</p>
-    <select>${moreThanOne}</select>`;
-  const maxChoices = products.max_quantity * "<option></option>";
+  const moreOptions = populateOptions(product.max_quantity);
+
+  divForProducts.innerHTML = `<img src="${product.img}" />
+  <h3>${product.name}</h3>
+  <p>${product.price}</p>
+  <select>
+      ${moreOptions}
+  </select>`;
+
   productsContainer.appendChild(divForProducts);
 });
 
-/// calculate the total cost of products
+/// calculate the total cost of products(cart)
 
 const allProducts = [...document.querySelectorAll(".product")];
-const getSum = () => {
+
+const total = () => {
   let sum = 0;
   allProducts.forEach((product, i) => {
     const productIndex = document.getElementById([i + 1]);
     const productQuantity = productIndex.querySelector("select").value;
     const productPrice = productIndex.querySelector("p").innerText;
-    cost = productPrice * productQuantity;
+    cost = productQuantity* productPrice;
     sum += cost;
   });
   return sum;
 };
 
-let fundsString = documet.getElementById("remaining").querySelector("span");
+let fundsString = document.getElementById("remaining").querySelector("span");
+
 const number = fundsString.innerText.match(/\d+/g);
 let budget = number[0];
 
-/// if you don't have enough money
+/// if you are too poor to afford stuff
 
-const tooPoor = documet.createElement("div");
+const tooPoor = document.createElement("div");
 tooPoor.innerHTML = `You are too poor to buy all that stuff`;
 tooPoor.classList.add("error");
-const remaining = docume.getElementById("remaining");
+const remaining = document.getElementById("remaining");
 
-let completeTotal;
-documet.querySelectorAll("select").forEach((option) => {
+/// show current situatin (in cart)
+
+let currentSum;
+document.querySelectorAll("select").forEach((option) => {
+  let latestValue = option.value;
+  console.log(latestValue);
+  option.addEventListener("change", (event) => {
+    currentSum = total();
+    let budgetDep = (budget - currentSum).toFixed(2);
+
+    if (budget < currentSum) {
+      cart.appendChild(tooPoor);
+
+      setTimeout(() => {
+        tooPoor.remove();
+      }, 2000);
+        
+      option.value = latestValue;
+      currentSum = total();
+    } else {
+      fundsString.innerHTML = "£" + budgetDep;
+    }
   
-})
+  });
+});
