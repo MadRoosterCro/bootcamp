13fcf054bed9138ac7278bb4aef83a32d226e378
unknown <dario.car.kz@gmail.com>
2021-03-17 11:56:08 +0100

    start of cipher

--- 51-js-ceasar-cipher/index.js
+++ 51-js-ceasar-cipher/index.js
@@ -1,6 +1,22 @@
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const alphabetCipher = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 
-const rot13 = () => {
-    
+const rot13 = (string, input, output) => {
+    let someString = "";
+    for (let i=0; i<string.length; i++) {
+        let stringIndex = input.indexOf(string[i]);
+        let alphabetValue = output[stringIndex];
+        if (alphabetValue === undefined) {
+            someString += "";
+        } else {
+            someString += alphabetValue;
+        }
+    }
+    someString = string(someString);
+    return someString;
 }
+
+const encryptButton = document.getElementById("encrypt")
+encryptButton.addEventListener("click", () => {
+    
+} )
