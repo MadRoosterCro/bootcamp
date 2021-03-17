d64b2e098716964b43ae617b7fdc22873757bee8
unknown <dario.car.kz@gmail.com>
2021-03-17 11:48:43 +0100

    start of cipher

---
+++ 51-js-ceasar-cipher/index.js
@@ -0,0 +1,6 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const alphabetCipher = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
+const rot13 = () => {
+    
+}
