2fc8fe35b5b057b9cec40e2ed4fc15400b2817cb
unknown <dario.car.kz@gmail.com>
2021-04-23 12:06:56 +0200

    new commit

--- 62-js-own-library-exercise/index.js
+++ 62-js-own-library-exercise/index.js
@@ -1,13 +1,13 @@
 const createATag = (tagName, className, id) => {
   const newTag = document.createElement(tagName);
+  if (tagName) {
+    return newTag;
+  }
   if (className) {
     newTag.classList.add(...className);
   }
   if (id) {
     newTag.id = id;
-  }
-  if (tagName) {
-    return newTag;
   } else {
     null;
   }
